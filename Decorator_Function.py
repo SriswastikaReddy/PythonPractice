600c07ca392ee9d7fea8398c72452adf50b6cce1
Sriswastika <swastikareddy02@gmail.com>
2026-02-17 23:17:42 -0600

    python dialy practice

---
+++ Decorator_Function.py
@@ -0,0 +1,25 @@
+#Decorator Function - Closure function + function as parameter
+
+
+def outer(f):
+    def inner():
+        print('*'* 10)
+        f()
+        print('*' * 10)
+    return inner
+
+@outer
+def new():
+    print('welcome')
+
+#r = outer(new)
+#r()
+
+#new = outer(new)
+new()
+
+
+
+
+
+
